34a0f4087700e7477cc31c5107e87337a603a289
Yuhang <yuhangj@andrew.cmu.edu>
2015-06-24 12:35:15 -0400

    add explanation of Jump Game II

--- Leetcode/DP/45_Jump_Game_II.java
+++ Leetcode/DP/45_Jump_Game_II.java
@@ -1,6 +1,14 @@
 public class Solution {
     // 不太像DP, 自己的版本
     // O(1) space, O(n) time
+    //   i
+    //  {7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3}
+    //   |____________________|
+    //    maxDistance(nums[0])
+    //   7, 7, 11,9,13,11, 7, 14 (nextDistance from i to maxDistance)
+    //   when i == maxDistance, step+=1 and update maxDistance = nextDistance
+    //   then next round, until maxDistance >= nums.length - 1
+
     public int jump(int[] nums) {
         if(nums.length <= 1)
             return 0;
@@ -10,9 +18,7 @@ public class Solution {
         for(int i = 1; i <= maxDistance; i++){
         	if(maxDistance >= nums.length - 1)
                 return step;
-        	if(nums[i] + i > nextDistance){
-            	nextDistance = nums[i] + i;
-            }
+            nextDistance = Math.max(nums[i] + i, nextDistance);
         	if(i == maxDistance){
             	step++;
             	maxDistance = nextDistance;
@@ -21,20 +27,20 @@ public class Solution {
         return 0;
     }
 
-    // 简洁版本, 别人家答案, 感觉自己家答案牛逼点
-    // 对于A = {0,0,0} 这种走不到最后的input, 此答案作废
-    public int jump2(int[] A) {
+    // 别人家解法, 感觉还是自己家解法牛逼点,因为对于nums = {0,0,0}这种走不到最后
+    // 的input, 此解法作废(突然觉得自己好机智)
+    public int jump2(int[] nums) {
         if(A.length <= 1)
             return 0;
         int step = 0;
         int MaxDist = 0;
         int NextMaxDist = 0;
-        for(int i = 0; i < A.length; i++){
+        for(int i = 0; i < nums.length; i++){
             if(i > MaxDist){
                 MaxDist = NextMaxDist;
                 step++;
             }
-            NextMaxDist = Math.max(NextMaxDist, i+A[i]);
+            NextMaxDist = Math.max(NextMaxDist, i + nums[i]);
         }
         return step;
     }
