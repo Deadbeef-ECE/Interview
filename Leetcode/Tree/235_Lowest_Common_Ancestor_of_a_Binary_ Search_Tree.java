b093440cb6791317ed4fb52a0e2df271dce00afa
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-10 17:54:48 -0400

    add top-down solution of 235 LCA of BST

--- Leetcode/Tree/235_Lowest_Common_Ancestor_of_a_Binary_ Search_Tree.java
+++ Leetcode/Tree/235_Lowest_Common_Ancestor_of_a_Binary_ Search_Tree.java
@@ -1,7 +1,7 @@
 public class Solution {
-	// Bottom-Up Solution:
-	// O(1) space OR O(logn) space if consider recursion stack
-	// O(n) time
+    // Bottom-Up Solution:
+    // O(1) space OR O(logn) space if consider recursion stack
+    // O(n) time
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null)    return null;
         if(root == p || root == q)  return root;
@@ -9,5 +9,18 @@ public class Solution {
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         if(left != null && right != null)   return root;
         return left != null ? left : right;
+    }
+
+    // Top-Down Solution:
+    // O(1) space OR O(logn) space if consider recursion stack
+    // O(logn) time
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return root;
+        }
     }
 }
