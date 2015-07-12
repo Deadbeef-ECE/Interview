6cda6f96bbe9861e3d0fa56fb22a2cb6728d404d
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-12 14:08:22 -0400

    add 236 LCA of a BT

--- Leetcode/Tree/235_Lowest_Common_Ancestor_of_a_Binary_ Search_Tree.java
+++ Leetcode/Tree/235_Lowest_Common_Ancestor_of_a_Binary_ Search_Tree.java
@@ -3,11 +3,12 @@ public class Solution {
     // O(1) space OR O(logn) space if consider recursion stack
     // O(n) time
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null)    return null;
-        if(root == p || root == q)  return root;
+        if(root == null || root == p || root == q)  
+            return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left != null && right != null)   return root;
+        if(left != null && right != null)   
+            return root;
         return left != null ? left : right;
     }
 
