cb247fa45ede94a3bbc1237c999f8d1a7cc2755a
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-17 16:54:43 -0400

    modified twitter/numberDP.java

--- Company/twitter/NumberDp.java
+++ Company/twitter/NumberDp.java
@@ -8,11 +8,10 @@ public class NumberDp {
 		if(target == 0)	return 0;
 		if(target == 1)	return 1;
 		if(target == 2) return 2;
-		if(target == 3)	return 2;
 		int[] dp = new int[target+1];
-		dp[0] = 0; dp[1] = 1; dp[2] = 2; dp[3] = 2;
-		for(int i = 4; i <= target; i++){
-			int firstBlood = Math.min(Math.min(dp[i-1]+1, dp[i-2]+2), dp[i-3]+3);
+		dp[0] = 0; dp[1] = 1; dp[2] = 2;
+		for(int i = 3; i <= target; i++){
+			int firstBlood = Math.min(dp[i-1]+1, dp[i-2]+2);
 			int doubleKill = Integer.MAX_VALUE;
 			int tripleKill = Integer.MAX_VALUE;
 			if((i / 3 % 1) == 0 && i % 3 == 0){
@@ -20,15 +19,12 @@ public class NumberDp {
 				doubleKill = Math.min(firstBlood, dp[temp] + 1);
 			}
 			if(logOfBase(3, i) % 1 == 0){
-//				System.out.println("wtf " + i + " "+ logOfBase(3, i));
 				tripleKill = Math.min(doubleKill, dp[1] + (int)logOfBase(3, i));
 			}
 			dp[i] = Math.min(Math.min(firstBlood, doubleKill), tripleKill);
 			System.out.println("i: "+i + ", dp[" + i + "] " + dp[i]);
-
 		}
 		return dp[target];
-
 	}
 	
 	public static double logOfBase(int base, int num) {
@@ -36,8 +32,8 @@ public class NumberDp {
 	}
 	
 	public static void main(String[] args){
-		int target = 27;
-		printRet("The min step to get target is: ", target);
+		int target = 100;
+		printRet("The minimum step to get target is: ", target);
 	}
 	
 	public static void printRet(String str, int target){
