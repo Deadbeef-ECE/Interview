93cad49ae10304a453d8992a4a62d44e6619f05d
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-19 14:47:12 -0400

    add subsets I II, TOOD: complexity analysis

---
+++ Leetcode/BackTracking/078_Subsets.java
@@ -0,0 +1,23 @@
+public class Solution {
+    // TODO: 
+    // O() time
+    // O() space
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ret = new LinkedList<List<Integer>>();
+        ret.add(new LinkedList<Integer>());
+        if(nums == null || nums.length == 0)    return ret;
+        Arrays.sort(nums);
+        List<Integer> path = new LinkedList<Integer>();
+        doDFS(0, nums, path, ret);
+        return ret;
+    }
+    
+    private void doDFS(int start, int[] nums, List<Integer> path, List<List<Integer>> ret){
+        for(int i = start; i < nums.length; i++){
+            path.add(nums[i]);
+            ret.add(new LinkedList<Integer>(path));
+            doDFS(i + 1, nums, path, ret);
+            path.remove(path.size() - 1);
+        }
+    }
+}
