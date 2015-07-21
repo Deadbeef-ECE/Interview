fe6a17e4d262dfb8ac338958ab76c20e3e539e47
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-20 23:10:09 -0400

    add Sudoku Solver, TODO: time & space complexity

---
+++ Leetcode/BackTracking/037_Sudoku_Solver.java
@@ -0,0 +1,52 @@
+public class Solution {
+    // TODO: 
+    // O() time
+    // O() space
+    public void solveSudoku(char[][] board) {
+        if(board.length != 9 || board[0].length != 9)
+            return;
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.'){
+                    for(char num = '1'; num <= '9'; num++){
+                        if(isValid(board, i, j, num)){
+                            board[i][j] = num;
+                            if(solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                        
+                    }
+                    // after tried all possible number, still cannot solve, return false
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char num){
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == num)
+                return false;
+        }
+        
+        for(int j = 0; j < 9; j++){
+            if(board[row][j] == num)
+                return false;
+        }
+        
+        for(int i = row/3*3; i < row/3*3+3; i++){
+            for(int j = col/3*3; j < col/3*3+3; j++){
+                if(board[i][j] == num)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
