3e99e64e051bdbea52192fcb0535ef46c3c7a64e
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-28 13:11:15 -0400

    update some questions in Company/你猜

--- Company/你猜/Q005_ComputeWithBigInt.java
+++ Company/你猜/Q005_ComputeWithBigInt.java
@@ -1,17 +1,50 @@
 // 5.
 // 实现任意大整数的加减乘除
 
+import java.math.BigInteger;
+
 public class Q005_ComputeWithBigInt {
-	public static double compute(int a, int b, char op){
-		double ret = 0;
-		return ret;
-	}
-	public static void main(String[] args){
-		printRet(10000, 100000, '+');
+	// Use BigInteger Class
+	public static String compute(String a, String b, char op){
+		BigInteger A = new BigInteger(a);
+		BigInteger B = new BigInteger(b);
+		switch (op) {
+		case '+':
+			A = A.add(B);
+			break;
+		case '-':
+			B = B.negate();
+			A = A.add(B);
+			break;
+		case '*':
+			A = A.multiply(B);
+			break;
+		case '/':
+			if(B.equals(new BigInteger("0"))){
+				System.out.print("Are you fucking kidding with me? = ");
+				return B.toString();
+			}
+			A = A.divide(B);
+			break;
+		default:
+			break;
+		}
+		return A.toString();
 	}
 	
-	private static void printRet(int a, int b, char op){
-		double ret = compute(a, b, op);
-		System.out.println(a);
+	// **************************************************
+	// TODO: What if we cannot use BigInteger Class ??
+	// **************************************************
+	
+	public static void main(String[] args){
+		test("2321321312124141", "21321312124141", '-');
+		test("2321321312124141", "0", '/');
+		test("2321321312124141", "21321312124141", '*');
+		test("2321321312124141", "21321312124141", '+');
+	}
+	
+	private static void test(String a, String b, char op){
+		System.out.print(a +" " + op + " " + b + " = ");
+		System.out.print(compute(a, b, op)+"\n");
 	}
 }
