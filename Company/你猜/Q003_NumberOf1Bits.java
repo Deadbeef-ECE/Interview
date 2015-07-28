cc5e89d769a55641b2f208262e70b27f62ab8f85
Yuhang <yuhangj@andrew.cmu.edu>
2015-07-28 13:14:45 -0400

    style modification of Q003

--- Company/你猜/Q003_NumberOf1Bits.java
+++ Company/你猜/Q003_NumberOf1Bits.java
@@ -10,18 +10,18 @@ public class Q003_NumberOf1Bits {
         }
         return ret;
     }
-	
-	public static void main(String[] args){
-		test(101);
-		test(999922);
-		test(2435234);
-		test(0);
-		test(Integer.MAX_VALUE);
-		test(Integer.MIN_VALUE);
+    
+    public static void main(String[] args){
+        test(101);
+        test(999922);
+        test(2435234);
+        test(0);
+        test(Integer.MAX_VALUE);
+        test(Integer.MIN_VALUE);
 	}
-	
-	private static void test(int n){
-		System.out.println("Input: " + n + " ("+Integer.toBinaryString(n) + ")");
-		System.out.println(n + " has " + NumberOfOnes(n) + " 1s\n");
-	}
+    
+    private static void test(int n){
+        System.out.println("Input: " + n + " ("+Integer.toBinaryString(n) + ")");
+        System.out.println(n + " has " + NumberOfOnes(n) + " 1s\n");
+    }
 }
