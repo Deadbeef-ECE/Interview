ebdad64a167ef90e99d5e012e28b5d39c9489657
Yuhang <yuhangj@andrew.cmu.edu>
2015-08-03 09:59:21 -0400

    add tarjanSCC.java and no test

---
+++ Algorithm&DataStructure/Graph/TarjanSCC.java
@@ -0,0 +1,75 @@
+import java.util.Stack;
+
+
+public class TarjanSCC {
+	private boolean[] marked;        // marked[v] = has v been visited?
+    private int[] id;                // id[v] = id of strong component containing v
+    private int[] low;               // low[v] = low number of v
+    private int pre;                 // preorder number counter
+    private int count;               // number of strongly-connected components
+    private Stack<Integer> stack;
+
+
+    /**
+     * Computes the strong components of the digraph <tt>G</tt>.
+     * @param G the digraph
+     */
+    public TarjanSCC(Digraph G) {
+        marked = new boolean[G.V()];
+        stack = new Stack<Integer>();
+        id = new int[G.V()]; 
+        low = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) dfs(G, v);
+        }
+    }
+
+    private void dfs(Digraph G, int v) { 
+        marked[v] = true;
+        low[v] = pre++;
+        int min = low[v];
+        stack.push(v);
+        for (int w : G.adj(v)) {
+            if (!marked[w]) dfs(G, w);
+            if (low[w] < min) min = low[w];
+        }
+        if (min < low[v]) { low[v] = min; return; }
+        int w;
+        do {
+            w = stack.pop();
+            id[w] = count;
+            low[w] = G.V();
+        } while (w != v);
+        count++;
+    }
+
+
+    /**
+     * Returns the number of strong components.
+     * @return the number of strong components
+     */
+    public int count() {
+        return count;
+    }
+
+
+    /**
+     * Are vertices <tt>v</tt> and <tt>w</tt> in the same strong component?
+     * @param v one vertex
+     * @param w the other vertex
+     * @return <tt>true</tt> if vertices <tt>v</tt> and <tt>w</tt> are in the same
+     *     strong component, and <tt>false</tt> otherwise
+     */
+    public boolean stronglyConnected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+    /**
+     * Returns the component id of the strong component containing vertex <tt>v</tt>.
+     * @param v the vertex
+     * @return the component id of the strong component containing vertex <tt>v</tt>
+     */
+    public int id(int v) {
+        return id[v];
+    }
+}
