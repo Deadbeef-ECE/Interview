bf6f61057f2dd3d8967b5b03611017965b61ca51
Yuhang <yuhangj@andrew.cmu.edu>
2015-09-24 19:53:36 -0700

    update some files

--- Leetcode/LinkedList/025_Reverse_Nodes_in_k-Group.java
+++ Leetcode/LinkedList/025_Reverse_Nodes_in_k-Group.java
@@ -32,4 +32,38 @@ public class Solution {
         }
         return cur;
     }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null || k <= 1)   return head;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        ListNode front = head.next;
+        while(front != null){
+            for(int i = 0; i < k - 2; i++){
+                front = front.next;
+                if(front == null)
+                    return dummy.next;
+            }
+            ListNode temp = front.next;
+            front.next = null;
+            pre.next = reverse(cur);
+            cur.next = temp;
+            pre = cur;
+            cur = temp;
+            front = temp == null ? null : temp.next;
+        }
+        return dummy.next;
+    }
+    
+    private ListNode reverse(ListNode node){
+        if(node == null)    return null;
+        if(node.next == null)   return node;
+        ListNode newHd = reverse(node.next);
+        node.next.next = node;
+        node.next = null;
+        return newHd;
+    }
 }
