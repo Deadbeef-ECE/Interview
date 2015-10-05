1ce5e3edff6245345c2e86e590ba5e288bca700a
Yuhang <yuhangj@andrew.cmu.edu>
2015-10-05 17:40:24 -0400

    add 牛逼solution of 214_Shortest_Palindrome.java

--- Leetcode/String/214_Shortest_Palindrome.java
+++ Leetcode/String/214_Shortest_Palindrome.java
@@ -1,4 +1,18 @@
 public class Solution {
+    public String shortestPalindrome(String s) {
+        int i = 0;
+        int end = s.length() - 1;
+        int j = end;
+        char chs[] = s.toCharArray();
+        while(i < j) {
+             if (chs[i] == chs[j]){
+                 i++; j--;
+             }else{ 
+                 i = 0; end--; j = end;
+             }
+        }
+        return new StringBuilder(s.substring(end+1)).reverse().toString() + s;
+    }
     // Cannot pass the shitest case
     // O(n) space, O(n^2) time
     public static String shortestPalindrome(String s) {
