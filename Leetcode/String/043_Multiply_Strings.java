ac44dc27b0df14cf57670e01ff34e370e102afa7
Yuhang <yuhangj@andrew.cmu.edu>
2015-10-04 21:07:22 -0400

    update some questions

--- Leetcode/String/043_Multiply_Strings.java
+++ Leetcode/String/043_Multiply_Strings.java
@@ -1,5 +1,31 @@
 public class Solution {
-    // O(n) space, O(n) time
+    // O(n) space, O(n^2) time
+    // No useless reverse
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0"))
+            return "" + 0;
+        int[] arr = new int[num1.length() + num2.length() - 1];
+        
+        for(int i = 0; i < num1.length(); i++){
+            for(int j = 0; j < num2.length(); j++){
+                arr[i+j] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+            }
+        }
+        
+        StringBuilder ret = new StringBuilder();
+        int carry = 0;
+        for(int i = arr.length - 1; i >= 0; i--){
+            int sum = arr[i] + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+            ret.insert(0, digit);
+        }
+        if(carry != 0)
+            ret.insert(0, carry);
+        return ret.toString();
+    }
+    
+    // O(n) space, O(n^2) time
     public String multiply(String num1, String num2) {
         String n1 = new StringBuilder(num1).reverse().toString();
         String n2 = new StringBuilder(num2).reverse().toString();
