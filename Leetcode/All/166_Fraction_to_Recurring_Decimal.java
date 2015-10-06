106dac42817c2ac7b415d052dde42305e331f924
Yuhang <yuhangj@andrew.cmu.edu>
2015-10-06 15:38:42 -0400

    update 287_Find_the_Duplicate_Number.java

--- Leetcode/All/166_Fraction_to_Recurring_Decimal.java
+++ Leetcode/All/166_Fraction_to_Recurring_Decimal.java
@@ -1,35 +1,33 @@
 public class Solution {
     // space, time?
     public String fractionToDecimal(int numerator, int denominator) {
-        if(numerator == 0)  return "0";
-        String ret = "";    
-        if ((numerator < 0) ^ (denominator < 0))
-            ret += "-";  
-        long num = Math.abs((long)numerator);  
-        long den = Math.abs((long)denominator);
-
-        long quotient = num/den;
+        if(numerator == 0)    return "0";
+        String ret = "";
+        if((numerator < 0) ^ (denominator < 0))
+            ret += "-";
+            
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+        
+        long quotient = num / den;
         long remainder = num % den;
-        ret += String.valueOf(quotient);
-        if(remainder == 0){
-            return ret;
-        }else{
-            ret += ".";
-        }
+        ret += quotient;
+        if(remainder == 0)   return ret;
+        ret += ".";
+        
         HashMap<Long, Integer> map = new HashMap<Long, Integer>();
-        remainder *= 10;
         while(remainder != 0){
+            remainder *= 10;
             if(map.containsKey(remainder)){
-                int pos = map.get(remainder);
-                String part1 = ret.substring(0, pos);
-                String part2 = ret.substring(pos, ret.length());
+                String part1 = ret.substring(0, map.get(remainder));
+                String part2 = ret.substring(map.get(remainder));
                 ret = part1 + "(" + part2 + ")";
                 return ret;
             }
-            map.put(remainder, ret.length());
             quotient = remainder / den;
-            ret += String.valueOf(quotient);
-            remainder = (remainder % den)*10;
+            map.put(remainder, ret.length());
+            ret += quotient;
+            remainder = remainder % den;
         }
         return ret;
     }
