6b2a8005bb0c1dfd9b6f0cc8692fa95ea45b87c7
Yuhang <yuhangj@andrew.cmu.edu>
2015-10-06 15:02:11 -0400

    Update Niubi Solution of Happy Number

--- Leetcode/All/202_Happy_Number.java
+++ Leetcode/All/202_Happy_Number.java
@@ -1,4 +1,26 @@
 public class Solution {
+    // NiuBi Solution
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = getSquareSum(slow);
+            fast = getSquareSum(fast);
+            fast = getSquareSum(fast);
+        } while(slow != fast);
+
+        return slow == 1;
+    }
+    
+    private int getSquareSum(int n){
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    
     // space, time?
     public boolean isHappy(int n) {
         if(n <= 0)  return false;
@@ -8,15 +30,13 @@ public class Solution {
             int temp = n;
             while(temp != 0){
                 int digit = temp % 10;
-                next += digit*digit;
+                next += digit * digit;
                 temp /= 10;
             }
             if(set.contains(next)){
                 return false;
-            }else{
-                set.add(next);
-                n = next;
-            }
+            set.add(next);
+            n = next;
         }
         return true;
     }
--- Leetcode/Math/202_Happy_Number.java
+++ Leetcode/Math/202_Happy_Number.java
@@ -1,4 +1,26 @@
 public class Solution {
+    // NiuBi Solution
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = getSquareSum(slow);
+            fast = getSquareSum(fast);
+            fast = getSquareSum(fast);
+        } while(slow != fast);
+
+        return slow == 1;
+    }
+    
+    private int getSquareSum(int n){
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    
     // space, time?
     public boolean isHappy(int n) {
         if(n <= 0)  return false;
